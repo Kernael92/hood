9ec7824dd958d9bf1bc86ea4b5f9b4250507ed43
Kernael92 <kernaelapuko5@gmail.com>
2019-01-10 00:20:20 +0300

    create Hood and profile model classes

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Hood(models.Model):
+    name = models.CharField(max_length=180)
+    location = models.CharField(max_length=180)
+    occupants_count = models.IntegerField()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_pi = models.ImageField(upload_to = 'profile_pic/')
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE)
+
