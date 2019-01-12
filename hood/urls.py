13b5d3e23d5d7677497a306050a5b477e677de9e
Kernael92 <kernaelapuko5@gmail.com>
2019-01-12 15:20:58 +0300

    create a template to hold the new business form

--- hood/urls.py
+++ hood/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^profile/(?P<username>[\w.@+-]+)/$', views.profile, name='profile'),
     url(r'^setting/', views.profile_setting, name='profile_setting'),
     url(r'^business/',views.business, name='business'),
+    url(r'^new/business$', views.new_business, name='new-business'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
